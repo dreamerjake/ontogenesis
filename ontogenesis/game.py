cc30ad3c584faf58dc7c01314964980cbf947ade
dreamerjake <jacobsilverman85@gmail.com>
2017-11-29 20:40:55 -0500

    update for player motion and addition of game.delta_time

--- ontogenesis/game.py
+++ ontogenesis/game.py
@@ -59,7 +59,9 @@ class Game:
 
         self.screen = pg.display.set_mode((settings.WIDTH, settings.HEIGHT))
         pg.display.set_caption(settings.TITLE)
+
         self.clock = pg.time.Clock()
+        self.delta_time = None
 
         # configs
         self.debug = settings.DEBUG
@@ -114,7 +116,7 @@ class Game:
         self.new()
 
         while True:
-            self.clock.tick()
+            self.delta_time = self.clock.tick(60) / 1000
             self.events()
             state_map[self.fsm.current_state]()
 
--- ontogenesis/player.py
+++ ontogenesis/player.py
@@ -11,9 +11,30 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((settings.TILESIZE, settings.TILESIZE))
         self.image.fill(settings.YELLOW)
         self.rect = self.image.get_rect()
-        self.x = start[0]
-        self.y = start[1]
-
-    def update(self):
+        self.vx, self.vy = 0, 0
+        self.x, self.y = start
         self.rect.x = self.x * settings.TILESIZE
         self.rect.y = self.y * settings.TILESIZE
+
+        # default stats
+        self.speed = 100
+
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vx = -self.speed
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = self.speed
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = -self.speed
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = self.speed
+
+    def move(self):
+        pass
+
+    def update(self):
+        self.get_keys()
+        self.rect.x += self.vx * self.game.delta_time
+        self.rect.y += self.vy * self.game.delta_time
