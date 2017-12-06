bd449f24f7c48329307ac666457d5c6d4e2241ca
dreamerjake <jacobsilverman85@gmail.com>
2017-12-05 21:02:36 -0500

    add require_attributes helper

--- ontogenesis/helpers.py
+++ ontogenesis/helpers.py
@@ -7,6 +7,12 @@ def require_methods(obj, method_list):
             raise Exception('Missing method {}'.format(method))
 
 
+def require_attributes(obj, attribute_list):
+    for attribute in attribute_list:
+        if not hasattr(obj, attribute):
+            raise Exception('Missing attribute {}'.format(attribute))
+
+
 def calc_dist(point1, point2):
     dist = sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
     return dist
